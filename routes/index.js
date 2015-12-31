872b0c79b111f3bab06578489da69af4c122fed3
Mauro Calvi <mauroc@yahoo.com>
2015-12-31 13:30:25 -0800

    copyright notices

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,20 @@
+/*
+ /*
+ * Copyright 2015 Mauro Calvi <coupdemistral@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var express = require('express');
 var router = express.Router();
 var isAuthenticated = require('../lib/isauth');
@@ -61,11 +78,13 @@ module.exports = function(passport){
     });
 
     router.get("/vessels/:id/navigation", function(req, res){
+        // navigation data for :id vessel.
         var id  = req.params["id"]  ;
         auth_req(req, res, authServer+"/signalk/api/v1/vessels/"+id+"/navigation");
     });
 
     router.get("/resources/:id/waypoints",  function(req, res){
+        // show list of squiddio waypoints near own vessel's current position
         var id  = req.params["id"] || 0 ;
         auth_req(req, res, authServer+"/signalk/api/v1/resources/"+id+"/waypoints");
     });
